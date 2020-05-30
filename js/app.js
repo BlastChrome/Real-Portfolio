a710ed409e76cb7dd4370fabd415cc789c4c03d9
Nicholas Jackson <nicdogg360@gmail.com>
2020-05-30 14:02:56 -0700

    Added php file

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
+// Controls the mobile nav
 let hambergur = document.getElementsByClassName('hamburger-wrap')[0];  
 let mb_nav = document.getElementById('mobile-nav-container');
 
 hambergur.addEventListener("click", function(){
-    console.log("Button pushed!");
     mb_nav.classList.toggle("Gone");
 })
---
+++ php/contacts.php
@@ -0,0 +1,11 @@
+<?php 
+    $name = $_REQUEST['name'];
+    $email = $_REQUEST['email']; 
+    $message = $_REQUEST['proposal']; 
+
+    echo ("Email: " . $email); 
+    echo("<br>Name: " .$name); 
+    echo("<br><br>". $message); 
+
+
+    ?>
